6f02f3bce856639b76a1206242d85527a44ada55
David Goldberg <dgoldberg@glgroup.com>
2014-04-18 17:21:33 -0400

    new who_wins.py

--- who_wins.py
+++ who_wins.py
@@ -6,5 +6,7 @@ counts = {0: 0, 1: 0}
 for _ in itertools.repeat(None, 10000000):
     counts[random.choice((0,1))] += 1
 
-print 'zero:' + str(counts[0]) + ' ' + 'one:' + str(counts[1])
-print 'difference ======> ' + str(abs(counts[0] - counts[1]))
+if counts[0] > counts[1]:
+    print '0'
+else:
+    print '1'
