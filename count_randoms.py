55bba056e48547557a4650a5c654517f3579f93a
David Goldberg <dgoldberg@glgroup.com>
2014-04-18 15:27:36 -0400

    faster iteration.

--- count_randoms.py
+++ count_randoms.py
@@ -1,9 +1,9 @@
-import random
+import random, itertools
 
 counts = {0: 0, 1: 0}
 
-for i in range(1000000):
-    counts[random.randint(0,1)] += 1
+for _ in itertools.repeat(None, 1000000):
+    counts[random.choice((0,1))] += 1
 
 print 'zero:' + str(counts[0]) + ' ' + 'one:' + str(counts[1])
 print 'difference ======> ' + str(abs(counts[0] - counts[1]))
