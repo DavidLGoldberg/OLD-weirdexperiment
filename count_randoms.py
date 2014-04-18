d208119cd298f2c5c450952a87a6e81018abc13d
David Goldberg <dgoldberg@glgroup.com>
2014-04-18 16:51:02 -0400

    goot amount for now.

--- count_randoms.py
+++ count_randoms.py
@@ -2,7 +2,7 @@ import random, itertools
 
 counts = {0: 0, 1: 0}
 
-for _ in itertools.repeat(None, 1000000):
+for _ in itertools.repeat(None, 10000000):
     counts[random.choice((0,1))] += 1
 
 print 'zero:' + str(counts[0]) + ' ' + 'one:' + str(counts[1])
