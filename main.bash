a80725c4dc71ee59ad5ddf410801bdec9380db44
David Goldberg <dgoldberg@glgroup.com>
2014-04-18 23:13:14 -0400

    Working framework.
    - I get 20:0 Same to different...
    - Figure out bug?

--- main.bash
+++ main.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-(parallel python ::: ./spam_a_random.py ./who_wins.py) > out; echo done; tail -n2 out
+for i in {1..20}
+do
+    (parallel python ::: ./spam_a_random.py ./who_wins.py) > out;
+    read spam <<< `sed -n '1p' out`;
+    read victor <<< `sed -n '2p' out`;
+
+    if [ $spam == $victor ]; then
+        ((same++))
+    else
+        ((different++))
+    fi
+done
+
+echo "Same: $same"
+echo "Different: $different"
