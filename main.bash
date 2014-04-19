9a80686e3dbba7aedad2be7eb85ccd15b0fcb41a
David Goldberg <dgoldberg@glgroup.com>
2014-04-18 23:20:04 -0400

    Fixed bug.
    - No interesting findings duh :(

--- main.bash
+++ main.bash
@@ -3,8 +3,8 @@
 for i in {1..20}
 do
     (parallel python ::: ./spam_a_random.py ./who_wins.py) > out;
-    read spam <<< `sed -n '1p' out`;
-    read victor <<< `sed -n '2p' out`;
+    read spam <<< `head -n1 out`;
+    read victor <<< `tail -n1 out`;
 
     if [ $spam == $victor ]; then
         ((same++))
